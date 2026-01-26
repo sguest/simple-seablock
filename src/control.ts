feb2b658c9cba07f323cb7e3711f8cb20aacddfc
sguest <spencer.b.guest@gmail.com>
2026-01-25 22:01:47 -0600

    compat: use Any Planet Start interface to disable crash site (#32)

--- src/control.ts
+++ src/control.ts
@@ -5,4 +5,10 @@ script.on_init(() => {
         remote.call('freeplay', 'set_disable_crashsite', true);
         remote.call('freeplay', 'set_skip_intro', true);
     }
+
+    // Any Planet Start clobbers the standard freeplay crash site and intro, but provides an alternate interface
+    if(remote.interfaces.APS !== null) {
+        remote.call('APS', 'set_disable_crashsite', true);
+        remote.call('APS', 'set_skip_intro', true);
+    }
 })
--- src/control/starting-items.ts
+++ src/control/starting-items.ts
@@ -1,5 +1,3 @@
-import { default_ship_parts } from 'crash-site';
-
 script.on_event(defines.events.on_player_created, e => {
     let player = game.players.get(e.player_index);
 
@@ -7,53 +5,7 @@ script.on_event(defines.events.on_player_created, e => {
     {
         player.get_main_inventory().clear();
     }
-
-    if(script.active_mods['any-planet-start']) {
-        // APS doesn't respect other mods disabling the crash site and intro, unless they run on_init first
-        // which we will never do since APS is a dependency
-        // so, remove the crash site and cancel the intro cutscene
-        // Ideally we'd do this in the aps-post-init event, but it seems to not be raised if a crash site is present
-        let surface = player.surface;
-        let shipParts = default_ship_parts();
-        let partNames: string[] = ['crash-site-spaceship', 'crash-site-fire-flame', 'crash-site-explosion-smoke'];
-        for(let part of shipParts)
-        {
-            if(part.variations) {
-                for(let i = 1; i <= part.variations; i++) {
-                    partNames.push(`${part.name}-${i}`);
-                }
-            }
-            else {
-                partNames.push(part.name);
-            }
-        }
-
-        for(let entity of surface.find_entities_filtered({name: partNames}))
-        {
-            entity.destroy({ raise_destroy: false });
-        }
-
-        if(player.controller_type === defines.controllers.cutscene) {
-            player.exit_cutscene();
-        }
-    }
 });
-
-// Due to APS's crash site shenanigans, need to re-clear the inventory after the cutscene is cancelled or we'll end up with a lingering burner drill
-if(script.active_mods['any-planet-start']) {
-    script.on_event(defines.events.on_cutscene_cancelled, e => {
-        if(!storage.apsInventoryCleared)
-        {
-            storage.apsInventoryCleared = true;
-            const player = game.players.get(e.player_index);
-
-            if(player.controller_type === defines.controllers.character)
-            {
-                player.get_main_inventory().clear();
-            }
-        }
-    })
-}
 
 script.on_event(defines.events.on_chunk_generated, e => {
     const position = e.position as { x: number, y: number };
